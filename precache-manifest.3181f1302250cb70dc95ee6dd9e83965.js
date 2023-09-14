1efb72f75856f15c086b3e2d59dcb74e31327928
Hai <hai.nguyenxuan@digicommercegroup.com>
2023-09-14 14:35:21 +0700

    Updates

---
+++ precache-manifest.3181f1302250cb70dc95ee6dd9e83965.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "36171a00af1a90ed9973",
+    "url": "/static/css/main.3e6429f3.chunk.css"
+  },
+  {
+    "revision": "36171a00af1a90ed9973",
+    "url": "/static/js/main.e76e8f5d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3d81ffe0e79df6fa2dda",
+    "url": "/static/js/2.c7f45e20.chunk.js"
+  },
+  {
+    "revision": "f6d838b7c2479a6dc254d4c95085e466",
+    "url": "/static/media/logo.f6d838b7.png"
+  },
+  {
+    "revision": "168d6f189b7eb25a310d199a3d495892",
+    "url": "/index.html"
+  }
+];
