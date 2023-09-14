0bdb6ce4210860bc7c325cffc21c9a4f7bd9fa50
Hai <hai.nguyenxuan@digicommercegroup.com>
2023-09-14 14:02:02 +0700

    edit algorithm

--- src/algorithm/index.ts
+++ src/algorithm/index.ts
@@ -1,26 +1,31 @@
 export function miniMaxSum(arr: number[]): void {
   arr.sort((a, b) => a - b);
-  const minSum: number = arr.slice(0, 4).reduce((acc, num) => acc + num, 0);
-  const maxSum: number = arr.slice(1).reduce((acc, num) => acc + num, 0);
+  const minSum = arr.slice(0, 4).reduce((acc, num) => acc + num, 0);
+  const maxSum = arr.slice(1).reduce((acc, num) => acc + num, 0);
   console.log(minSum, maxSum);
 }
 
-export function getTotal(arr: number[]): number {
-  return arr.reduce((acc, num) => acc + num, 0);
+export function getTotal(arr: number[]): void {
+  const result = arr.reduce((acc, num) => acc + num, 0);
+  console.log("Total= ", result);
 }
 
-export function findMin(arr: number[]): number {
-  return Math.min(...arr);
+export function findMin(arr: number[]): void {
+  const result = Math.min(...arr);
+  console.log("Min= ", result);
 }
 
-export function findMax(arr: number[]): number {
-  return Math.max(...arr);
+export function findMax(arr: number[]): void {
+  const result = Math.max(...arr);
+  console.log("Max= ", result);
 }
 
-export function findEvenElements(arr: number[]): number[] {
-  return arr.filter((num) => num % 2 === 0);
+export function findEvenElements(arr: number[]): void {
+  const result = arr.filter((num) => num % 2 === 0);
+  console.log("Even= ", result);
 }
 
-export function findOddElements(arr: number[]): number[] {
-  return arr.filter((num) => num % 2 !== 0);
+export function findOddElements(arr: number[]): void {
+  const result = arr.filter((num) => num % 2 !== 0);
+  console.log("Odd= ", result);
 }
